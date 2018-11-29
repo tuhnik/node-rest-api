43f36dfb8d142dc0b623a6e847c2729067b23420
tuhnik <huntlarsen@gmail.com>
2018-11-29 18:17:45 +0200

    email handling

--- api/utils/mailsender.js
+++ api/utils/mailsender.js
@@ -2,8 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth: {         
-        
+    auth: {   
            user: process.env.GMAIL_USER,
            pass: process.env.GMAIL_PASSWORD
        }
