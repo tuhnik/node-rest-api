477feef9e139a23a2b4f598536b89db797c0a779
tuhnik <huntlarsen@gmail.com>
2018-12-05 07:21:34 +0200

    minor

--- api/middlewares/token-check.js
+++ api/middlewares/token-check.js
@@ -1,14 +1,24 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
         const decoded = jwt.verify(token, process.env.JWT_KEY)
         req.userData = decoded
-        next()
+        User.findOne({email: decoded.email}).then(user => {
+            if(user) { 
+                next()
+            }
+            else {
+                res.status(401).json({error: 'Authentication failed'})
+            }
+        })
+
+        
     } catch (error) {
         return res.status(401).json({
-            message: 'Authentication failed'
+            error: 'Authentication failed'
         })
     }
     
